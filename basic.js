ddc5f82e635cba26bf48a19b71387566190fd319
vanshika <139586612+98vanshika@users.noreply.github.com>
2024-07-07 11:19:55 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,5 +1,21 @@
-class Pen{
-String color;
-String type; // ballpoint;gel
+class Pen // defined
+{
+  String color;
+  String type; // ballpoint;gel
 
+  public void write() //method => function define under the class 
+  {     
+    System.out.print("Writing");
+  }
+}
+
+public class OOPS // main function
+{
+  public static void main(String args[]){
+    Pen pen1 = new Pen(); //object
+    pen1.color = "blue";  // properties of object is used by using dott "."
+    pen1.type = "gel";    // properties of object is used by using dott "."
+
+    pen1.write();
+  }
 }
